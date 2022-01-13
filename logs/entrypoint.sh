7119a9f260a0128729b9a1507d2e6416039d95f2
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-01-13 01:20:55 +0100

    no message

--- logs/entrypoint.sh
+++ logs/entrypoint.sh
@@ -2,7 +2,9 @@
 
 rm -f /data/server.log
 
-for i in $(seq 1 1000000); do
-  MESSAGE="$(date +'%Y-%m-%d %H:%M:%S.%N') INFO Went to home $(date +'%Y-%m-%d %H:%M:%S.%N')"
+RANDOM="`tr -dc A-Za-z0-9 </dev/urandom | head -c 1024 ; echo ''`"
+
+for i in $(seq 1 100000); do
+  MESSAGE="$(date +'%Y-%m-%d %H:%M:%S.%N') ${RANDOM}"
   echo "${MESSAGE}" >>/data/server.log
 done
